3d358801f0176bf50686ae01d3622e05f4bd30fa
Slava Kalmykov <slavafort35@gmail.com>
2024-08-04 21:22:21 +0300

    created login_get method, login_get form

--- todolist/routes.py
+++ todolist/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 from todolist import app
+from todolist.forms import LoginForm
 
 
 @app.route('/')
@@ -7,4 +8,16 @@ def return_main_page():
     return render_template('main_page.html')
 
 
-# @app.route()
+@app.route('/login', methods=['GET'])
+def login_user_get():
+    login_form = LoginForm(request.form)
+    return render_template('login_register.html', login_tab=True, login_form=login_form)
+
+
+@app.route('/login', methods=['POST'])
+def login_user_post():
+    login_form = LoginForm(request.form)
+
+    if login_form.validate_on_submit():
+        nick_name = login_form.nick_name.data
+        password = login_form.password.data
